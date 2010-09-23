3775300f689d6ca997975c518db38b6b5dbb1ccf
Steffen Mueller <smueller@cpan.org>
2010-09-23 10:58:20 +0200

    version bump, changelog

--- lib/Statistics/CaseResampling.pm
+++ lib/Statistics/CaseResampling.pm
@@ -14,7 +14,7 @@ our @EXPORT_OK = qw(
 our @EXPORT = qw();
 our %EXPORT_TAGS = ('all' => \@EXPORT_OK);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 require XSLoader;
 XSLoader::load('Statistics::CaseResampling', $VERSION);
